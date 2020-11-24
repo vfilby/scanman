1b1209def1596e1f82deb442ed4d2e73eb77b52d
Vincent Filby <v@filby.ca>
2020-11-23 22:22:57 -0800

    Basic queue/worker in addition to the file watcher.

--- scanman-server/app/scanman.py
+++ scanman-server/app/scanman.py
@@ -3,19 +3,19 @@ import os
 import sys
 import time
 import logging
+import multiprocessing
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
 from watchdog.events import FileSystemEventHandler
 
+tasks = multiprocessing.JoinableQueue()
+
 class FileWatcher:
-
-
     def __init__(self, watch_path ):
         self.observer = Observer()
         self.watchDirectory = watch_path
 
     def run(self):
-        event_handler = Handler()
+        event_handler = FileWatcherEventHandler()
         self.observer.schedule(event_handler, self.watchDirectory, recursive = False)
         self.observer.start()
         print( "observer started" )
@@ -29,36 +29,53 @@ class FileWatcher:
         self.observer.join()
 
 
-class Handler(FileSystemEventHandler):
+class FileWatcherEventHandler(FileSystemEventHandler):
+
+
+    def __init__( self ):
+        FileSystemEventHandler.__init__(self)
 
     @staticmethod
     def on_any_event(event):
-        print( event.event_type )
-        if event.is_directory:
-            return None
-        else:
-            print("Watchdog received %s event - % s." % (str(event), event.src_path) )
+        print("Watchdog received %s event - % s." % (event.event_type, event.src_path) )
+        tasks.put( event )
 
 
+class Worker(multiprocessing.Process):
+
+    def __init__( self, task_queue ):
+        multiprocessing.Process.__init__(self)
+        self.task_queue = task_queue
+
+    def run(self):
+        print( "Starting worker cycles" )
+        proc_name = self.name
+        while True:
+            print( "get task" )
+            next_task = self.task_queue.get()
+            if next_task is None:
+                # sit tight, have a nap and try again.
+                time.sleep(5)
+                print( "continuing" )
+                continue
+            print( next_task )
+            self.task_queue.task_done()
+        return
 
 
 if __name__ == "__main__":
+
     print( "Launching watcher for %s" % os.environ['INTAKE_DIR'])
+
+
     watch = FileWatcher( os.environ['INTAKE_DIR'] )
+
+    print( "starting worker" )
+
+    w = Worker( tasks )
+    w.start()
+    print( "worker started" )
+
+    # blocks until file watcher is stopped.
+    print( "starting watcher" )
     watch.run()
-    # logging.basicConfig(level=logging.INFO,
-    #                     format='%(asctime)s - %(message)s',
-    #                     datefmt='%Y-%m-%d %H:%M:%S')
-    # # path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    # path = os.environ['INTAKE_DIR']
-    # event_handler = LoggingEventHandler()
-    # #event_handler = Handler()
-    # observer = Observer()
-    # observer.schedule(event_handler, path, recursive=False)
-    # observer.start()
-    # try:
-    #     while True:
-    #         time.sleep(1)
-    # finally:
-    #     observer.stop()
-    #     observer.join()
